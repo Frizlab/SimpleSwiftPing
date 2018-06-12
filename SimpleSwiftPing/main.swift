4f18b49c093c43b0c335c9a6a46a60d7c9974e84
Frizlab <fload@me.com>
2018-06-12 23:32:45 +0200

    Initial Commit

---
+++ SimpleSwiftPing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleSwiftPing
+//
+//  Created by François Lamboley on 12/06/2018.
+//  Copyright © 2018 Frizlab. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
