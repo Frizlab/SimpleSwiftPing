adba4c6527e5182c3506ee571ed68b1a9f8cf825
Frizlab <fload@me.com>
2018-06-16 16:25:25 +0200

    Implement SimplePing

--- SimpleSwiftPing/main.swift
+++ SimpleSwiftPing/main.swift
@@ -1,12 +1,49 @@
-//
-//  main.swift
-//  SimpleSwiftPing
-//
-//  Created by François Lamboley on 12/06/2018.
-//  Copyright © 2018 Frizlab. All rights reserved.
-//
+/*
+ * main.swift
+ * SimpleSwiftPing
+ *
+ * Created by François Lamboley on 13/06/2018.
+ * Copyright © 2018 Frizlab. All rights reserved.
+ */
 
 import Foundation
 
-print("Hello, World!")
 
+
+class PingDelegate : SimplePingDelegate {
+	
+	func simplePing(_ pinger: SimplePing, didStart address: Data) {
+		print("ping start with address" + address.reduce("", { $0 + " " + String($1) }))
+		pinger.sendPing(data: nil)
+	}
+	
+	func simplePing(_ pinger: SimplePing, didFail error: Error) {
+		print("ping failed with error \(error)")
+	}
+	
+	func simplePing(_ pinger: SimplePing, didSendPacket packet: Data, sequenceNumber: UInt16) {
+		print("ping did send packet (sequence number \(sequenceNumber)): \(packet.reduce("", { $0 + String(format: "%02x", $1) }))")
+	}
+	
+	func simplePing(_ pinger: SimplePing, didFailToSendPacket packet: Data, sequenceNumber: UInt16, error: Error) {
+		print("ping did fail to send packet (sequence number \(sequenceNumber)) with error \(error). Packet: \(packet.reduce("", { $0 + String(format: "%02x", $1) }))")
+	}
+	
+	func simplePing(_ pinger: SimplePing, didReceivePingResponsePacket packet: Data, sequenceNumber: UInt16) {
+		print("ping did receive ping response packet (sequence number \(sequenceNumber)): \(packet.reduce("", { $0 + String(format: "%02x", $1) }))")
+	}
+	
+	func simplePing(_ pinger: SimplePing, didReceiveUnexpectedPacket packet: Data) {
+		print("ping did receive unexpected packet: \(packet.reduce("", { $0 + String(format: "%02x", $1) }))")
+	}
+	
+}
+
+let delegate = PingDelegate()
+let p = SimplePing(hostName: "9.9.9.9")
+p.delegate = delegate
+p.start()
+
+repeat {
+	RunLoop.current.run(mode: RunLoopMode.defaultRunLoopMode, before: Date.distantFuture)
+} while true
